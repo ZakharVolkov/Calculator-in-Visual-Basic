de0c13ffaaa52014517ef9d5da73387398e6b946
Zakhar <162204706+ZakharVolkov@users.noreply.github.com>
2024-06-03 13:56:10 +0300

    Add files via upload

---
+++ Source code/Calc/Form1.Designer.vb
@@ -0,0 +1,395 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        Label2 = New Label()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox1.Location = New Point(10, 34)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(294, 58)
+        TextBox1.TabIndex = 0
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaptionText
+        Button1.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.ButtonHighlight
+        Button1.Location = New Point(10, 133)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(69, 61)
+        Button1.TabIndex = 1
+        Button1.Text = "7"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ActiveCaptionText
+        Button2.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.ForeColor = SystemColors.ButtonHighlight
+        Button2.Location = New Point(84, 133)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(69, 61)
+        Button2.TabIndex = 2
+        Button2.Text = "8"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ActiveCaptionText
+        Button3.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.ForeColor = SystemColors.ButtonHighlight
+        Button3.Location = New Point(159, 133)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(69, 61)
+        Button3.TabIndex = 3
+        Button3.Text = "9"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.ActiveCaptionText
+        Button4.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button4.ForeColor = SystemColors.ButtonHighlight
+        Button4.Location = New Point(159, 200)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(69, 61)
+        Button4.TabIndex = 6
+        Button4.Text = "6"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = SystemColors.ActiveCaptionText
+        Button5.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.ForeColor = SystemColors.ButtonHighlight
+        Button5.Location = New Point(84, 200)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(69, 61)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = SystemColors.ActiveCaptionText
+        Button6.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button6.ForeColor = SystemColors.ButtonHighlight
+        Button6.Location = New Point(10, 200)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(69, 61)
+        Button6.TabIndex = 4
+        Button6.Text = "4"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = SystemColors.ActiveCaptionText
+        Button7.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button7.ForeColor = SystemColors.ButtonHighlight
+        Button7.Location = New Point(159, 267)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(69, 61)
+        Button7.TabIndex = 9
+        Button7.Text = "3"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = SystemColors.ActiveCaptionText
+        Button8.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button8.ForeColor = SystemColors.ButtonHighlight
+        Button8.Location = New Point(84, 267)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(69, 61)
+        Button8.TabIndex = 8
+        Button8.Text = "2"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = SystemColors.ActiveCaptionText
+        Button9.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button9.ForeColor = SystemColors.ButtonHighlight
+        Button9.Location = New Point(10, 267)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(69, 61)
+        Button9.TabIndex = 7
+        Button9.Text = "1"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = SystemColors.ActiveCaptionText
+        Button10.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button10.ForeColor = SystemColors.ButtonHighlight
+        Button10.Location = New Point(159, 334)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(69, 61)
+        Button10.TabIndex = 12
+        Button10.Text = "+/-"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = SystemColors.ActiveCaptionText
+        Button11.Font = New Font("Century Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Button11.ForeColor = SystemColors.ButtonHighlight
+        Button11.Location = New Point(84, 334)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(69, 61)
+        Button11.TabIndex = 11
+        Button11.Text = ","
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = SystemColors.ActiveCaptionText
+        Button12.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button12.ForeColor = SystemColors.ButtonHighlight
+        Button12.Location = New Point(10, 334)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(69, 61)
+        Button12.TabIndex = 10
+        Button12.Text = "0"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = SystemColors.ActiveCaptionText
+        Button13.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button13.ForeColor = SystemColors.ButtonHighlight
+        Button13.Location = New Point(234, 133)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(69, 61)
+        Button13.TabIndex = 13
+        Button13.Text = "+"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = SystemColors.ActiveCaptionText
+        Button14.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button14.ForeColor = SystemColors.ButtonHighlight
+        Button14.Location = New Point(308, 133)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(69, 61)
+        Button14.TabIndex = 14
+        Button14.Text = "-"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = SystemColors.ActiveCaptionText
+        Button15.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button15.ForeColor = SystemColors.ButtonHighlight
+        Button15.Location = New Point(308, 200)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(69, 61)
+        Button15.TabIndex = 16
+        Button15.Text = "÷"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = SystemColors.ActiveCaptionText
+        Button16.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button16.ForeColor = SystemColors.ButtonHighlight
+        Button16.Location = New Point(234, 200)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(69, 61)
+        Button16.TabIndex = 15
+        Button16.Text = "×"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = SystemColors.ActiveCaptionText
+        Button17.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button17.ForeColor = SystemColors.ButtonHighlight
+        Button17.Location = New Point(308, 267)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(69, 61)
+        Button17.TabIndex = 18
+        Button17.Text = "x²"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = SystemColors.ActiveCaptionText
+        Button18.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button18.ForeColor = SystemColors.ButtonHighlight
+        Button18.Location = New Point(234, 267)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(69, 61)
+        Button18.TabIndex = 17
+        Button18.Text = "√"
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = SystemColors.ActiveCaptionText
+        Button19.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button19.ForeColor = SystemColors.ButtonHighlight
+        Button19.Location = New Point(234, 334)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(144, 61)
+        Button19.TabIndex = 19
+        Button19.Text = "="
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.BackColor = SystemColors.ActiveCaptionText
+        Button20.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button20.ForeColor = SystemColors.ButtonHighlight
+        Button20.Location = New Point(10, 99)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(69, 28)
+        Button20.TabIndex = 20
+        Button20.Text = "C"
+        Button20.UseVisualStyleBackColor = False
+        ' 
+        ' Button21
+        ' 
+        Button21.BackColor = SystemColors.ActiveCaptionText
+        Button21.Font = New Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button21.ForeColor = SystemColors.ButtonHighlight
+        Button21.Location = New Point(84, 99)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(69, 28)
+        Button21.TabIndex = 21
+        Button21.Text = "CE"
+        Button21.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ButtonHighlight
+        Label2.Location = New Point(308, 22)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(69, 70)
+        Label2.TabIndex = 23
+        Label2.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(248, 10)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(55, 22)
+        Label1.TabIndex = 24
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(391, 404)
+        Controls.Add(Label1)
+        Controls.Add(Label2)
+        Controls.Add(Button21)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(Button17)
+        Controls.Add(Button18)
+        Controls.Add(Button15)
+        Controls.Add(Button16)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button10)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button7)
+        Controls.Add(Button8)
+        Controls.Add(Button9)
+        Controls.Add(Button4)
+        Controls.Add(Button5)
+        Controls.Add(Button6)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ Source code/Calc/Form1.vb
@@ -0,0 +1,146 @@
+﻿Imports System.Drawing.Text
+
+Public Class Form1
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If String.IsNullOrEmpty(TextBox1.Text) AndAlso String.IsNullOrEmpty(Label1.Text) Then
+            TextBox1.Text = "0,"
+        Else
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text + "1"
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text + "2"
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text + "3"
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text + "4"
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text + "5"
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text + "6"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text + "7"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text + "8"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text + "9"
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text + ","
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If String.IsNullOrEmpty(TextBox1.Text) Then
+            TextBox1.Text = ""
+        Else
+            TextBox1.Text = TextBox1.Text * (-1)
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "+"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "-"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "x"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "÷"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        TextBox1.Text = Math.Sqrt(TextBox1.Text)
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        TextBox1.Text = TextBox1.Text * TextBox1.Text
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Dim number1 As Decimal
+        Dim number2 As Decimal
+
+        If Decimal.TryParse(Label1.Text, number1) AndAlso Decimal.TryParse(TextBox1.Text, number2) Then
+            If Label2.Text = "+" Then
+                TextBox1.Text = (number1 + number2).ToString()
+                Label1.Text = ""
+                Label2.Text = "="
+            ElseIf Label2.Text = "-" Then
+                TextBox1.Text = (number1 - number2).ToString()
+                Label1.Text = ""
+                Label2.Text = "="
+            ElseIf Label2.Text = "x" Then
+                TextBox1.Text = (number1 * number2).ToString()
+                Label1.Text = ""
+                Label2.Text = "="
+            ElseIf Label2.Text = "÷" Then
+                If number2 <> 0 Then
+                    TextBox1.Text = (number1 / number2).ToString()
+                    Label1.Text = ""
+                    Label2.Text = "="
+                Else
+                    TextBox1.Text = "Error"
+                    Label1.Text = ""
+                    Label2.Text = ""
+                End If
+            End If
+        Else
+            TextBox1.Text = "Error"
+        End If
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Label1.Text = ""
+        Label2.Text = ""
+        TextBox1.Text = ""
+    End Sub
+
+End Class
